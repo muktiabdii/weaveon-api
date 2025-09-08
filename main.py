a3ff3db234bd193ea416554fe61118a76580b8b1
muktiabdii <abdisyukur10@gmail.com>
2025-09-08 22:36:47 +0700

    fix: fixing failed deploy in railway

--- main.py
+++ main.py
@@ -113,4 +113,4 @@ def analyze():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
